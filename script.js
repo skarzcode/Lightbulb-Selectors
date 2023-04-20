5cbdd66ef1429f0840fb915a759717fe407d03b6
Abdi Majid <abdi.amajid.dev>
2023-04-20 10:59:56 +0100

    finished excercise

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Write your code here
+let lightbulb = document.querySelectorAll(".lightbulb");
+let count = 0
+let span = document.querySelector("span")
+
+
+lightbulb.forEach(element => {
+    element.addEventListener("click", function(){
+       if(element.classList.contains('active')){
+        count ++;
+        element.classList.remove("active")
+       } else{
+        count ++;
+        element.classList.add("active")
+       }
+       span.innerHTML = count
+    })
+});
